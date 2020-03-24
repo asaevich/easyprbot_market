9cd9e57a2e02c0e8fdced3ddf89ac8e446b266dc
asaevich <semen.asaevich@gmail.com>
2020-03-24 21:56:10 +0300

    Add mask model

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from market.models import Mask
+
+admin.site.register(Mask)
--- market/models.py
+++ market/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Mask(models.Model):
+    is_enable = models.BooleanField('Включена ли', default=True)
+    name = models.CharField('Название', max_length=50,
+                            unique=True, blank=False, null=False)
+    description = models.TextField('Описание', max_length=400,
+                                   blank=False, null=False)
+    price = models.FloatField('Обычная цена', null=False)
+    discounted_price = models.FloatField('Цена со скидкой', null=True)
+    video_link = models.URLField('Ссылка на видео', null=True)
+    photo = models.ImageField('Загрузка фото', upload_to='masks/', null=False)
+    category = models.ManyToManyField('Category', verbose_name='Категория')
+    creator = models.ForeignKey('Creator', on_delete=models.PROTECT,
+                                verbose_name='Создатель маски')
+
+    class Meta:
+        verbose_name = 'Маска'
+        verbose_name_plural = 'Маски'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    pass
+
+
+class Creator(models.Model):
+    pass
