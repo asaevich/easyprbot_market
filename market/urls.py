73c934169d9dd75726e4124564864ee2ea3c0248
asaevich <semen.asaevich@gmail.com>
2020-04-17 20:59:58 +0300

    Add index view

--- market/urls.py
+++ market/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
 ]
