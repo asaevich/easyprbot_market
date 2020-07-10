e7049243e5e29d139f2c0c59897fe1558e9d77e3
asaevich <semen.asaevich@gmail.com>
2020-07-11 01:16:03 +0300

    Refactor urls, write product_list view

--- market/urls.py
+++ market/urls.py
@@ -1,29 +1,13 @@
-"""easyprbot_market URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
+app_name = 'market'
+
 urlpatterns = [
-    url('mask/', views.mask_list, name='mask_list'),
-    url('mask/<slug:category_slug>/', views.mask_list,
-        name='mask_list_by_category'),
-    url('mask/<int:id>/<slug:slug>/', views.mask_detail, name='mask_detail'),
-    url('filter/', views.filter_list, name='filter_list'),
-    url('filter/<slug:category_slug>/', views.filter_list,
-        name='filter_list_by_category'),
-    url('filter/<int:id>/<slug:slug>/', views.filter_detail,
-        name='filter_detail'),
+    path('order-product', views.order_product, name='order_product'),
+    path('<slug:product_type_slug>', views.product_list, name='product_list'),
+    path('<slug:product_type_slug>/<slug:category_slug>', views.product_list,
+         name='product_list_by_category'),
+    path('<slug:product_type_slug>/<int:id>/<slug:product_slug>',
+         views.product_detail, name='product_detail'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,30 +1,51 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Mask, Filter, Category
 
 
-def mask_list(request, category_slug=None):
-    category = None
+def product_list(request, product_type_slug, category_slug=None):
+    context = {}
+    selected_category = None
     categories = Category.objects.all()
-    masks = Mask.objects.filter(is_enable=True)
+
+    if product_type_slug == 'mask':
+        products = Mask.objects.filter(is_available=True)
+        css_class = 'first'
+    elif product_type_slug == 'filter':
+        products = Filter.objects.filter(is_available=True)
+        css_class = 'second'
+    else:
+        raise Http404('Страница не найдена')
 
     if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        masks = masks.filter(category=category)
+        selected_category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=selected_category)
 
-    return render(request, 'mask-list.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': masks})
+    for product in products:
+        product.preview = product.photos.filter(is_preview=True)[0].photo
+
+    paginator = Paginator(products, 2)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+
+    context = {'selected_category': selected_category,
+               'categories': categories,
+               'products': page.object_list,
+               'product_type': product_type_slug,
+               'pick_class': css_class,
+               'page': page}
+
+    return render(request, 'product-list.html', context)
 
 
-def filter_list(request, slug=None):
-    return render(request, 'filters-main.html')
-
-
-def mask_detail(request, id, slug):
+def product_detail(request, product_type_slug, id, product_slug):
     pass
 
 
-def filter_detail(request, id, slug):
-    pass
+def order_product(request):
+    return render(request, 'order-product.html')
